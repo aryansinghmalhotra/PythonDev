0887faced1cab5bd68c0d5410738e877778e6814
aryansinghmalhotra <140875369+aryansinghmalhotra@users.noreply.github.com>
2024-02-10 19:38:03 +0000

    search joke

--- JokeGPT/joke.py
+++ JokeGPT/joke.py
@@ -1,11 +1,30 @@
 import requests
 
-def get_joke():
-    response = requests.get('https://official-joke-api.appspot.com/jokes/programming/random')
+def get_joke(category, flag):
+    response = requests.get(f'https://v2.jokeapi.dev/joke/{category}?blacklistFlags={flag}')
     if response.status_code == 200:
-        joke = response.json()[0]
-        return f"{joke['setup']} - {joke['punchline']}"
+        data = response.json()
+        if data['type'] == 'single':
+            return data['joke']
+        elif data['type'] == 'twopart':
+            return f"{data['setup']} - {data['delivery']}"
     else:
         return "Couldn't retrieve a joke"
 
-print(get_joke())
+#print(get_joke("Programming", "explicit"))
+
+def search_joke(term):
+    response = requests.get(f'https://v2.jokeapi.dev/joke/Any?term={term}')
+    if response.status_code == 200:
+        data = response.json()
+        if data['type'] == 'single':
+            return data['joke']
+        elif data['type'] == 'twopart':
+            return f"{data['setup']} - {data['delivery']}"
+        else:
+            return "No jokes found"
+    else:
+        return "Couldn't retrieve a joke"
+
+print(search_joke("programmer"))
+
